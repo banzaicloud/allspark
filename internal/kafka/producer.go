0744276871a65067c79c6614703356f12f14081b
Balazs Berta <balazs.berta.dev@gmail.com>
2022-10-12 14:17:41 +0200

    Remove kafka bootstrap server defaulting, remove auto topic creation

--- internal/kafka/producer.go
+++ internal/kafka/producer.go
@@ -18,10 +18,6 @@ type Producer struct {
 }
 
 func NewProducer(bootStrapServer string, topic string, logger log.Logger) *Producer {
-	if bootStrapServer == "" {
-		bootStrapServer = "kafka-all-broker.kafka:29092"
-	}
-
 	return &Producer{
 		bootstrapServer: bootStrapServer,
 		topic:           topic,
@@ -32,9 +28,10 @@ func NewProducer(bootStrapServer string, topic string, logger log.Logger) *Produ
 func (p *Producer) Produce(ctx context.Context, message string) error {
 	if p.writer == nil {
 		p.writer = &kafka.Writer{
-			Topic:  p.topic,
-			Addr:   kafka.TCP(p.bootstrapServer),
-			Logger: p.logger,
+			Topic:                  p.topic,
+			Addr:                   kafka.TCP(p.bootstrapServer),
+			Logger:                 p.logger,
+			AllowAutoTopicCreation: false,
 		}
 	}
 
